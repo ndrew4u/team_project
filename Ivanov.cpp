8979dbca7a028c9f387b7b9b5e094dae62321d43
ndrew4u <aivanov.2005@mail.ru>
2026-09-15 20:48:58 +0700

    feat: перенесены функции варианта 32, добавлены функции меню 1-2

---
+++ Ivanov.cpp
@@ -0,0 +1,13 @@
+#include "Ivanov.h"
+
+const double g = 9.81;
+
+double gravityForce(double m)
+{
+    return m * g;
+}
+
+double weightInLift(double m, double a)
+{
+    return m * (g + a);
+}
--- main.cpp
+++ main.cpp
@@ -4,22 +4,50 @@
 #include<windows.h>
 using namespace std;
 // === БЛОК ПОДКЛЮЧЕНИЙ: каждый участник добавляет свой #include ===
+#include "Ivanov.h"
 // === КОНЕЦ БЛОКА ПОДКЛЮЧЕНИЙ ===
 int main()
 {
 	SetConsoleOutputCP(CP_UTF8);
     	SetConsoleCP(CP_UTF8);
 	int choice;
+	double m;
+	double a;
+	double result;
 	do
 	{
 		// === БЛОК МЕНЮ: каждый участник добавляет свои пункты ===
 		cout << "\n=== Командный проект: сборник расчётов ===\n";
+		cout << "1. Сила тяжести (F = m * g)\n";
+		cout << "2. Вес в лифте (P = m * (g + a) )\n";
 		cout << "0. Выход" << "\n";
 		cout << "Ваш выбор: ";
 		// === КОНЕЦ БЛОКА МЕНЮ ===
 		cin >> choice;
 		switch (choice)
 		{ // === БЛОК ОБРАБОТКИ: каждый участник добавляет свои case ===
+		case 1:
+			do
+			{
+				cout << "Введите массу (m): ";
+				cin >> m;
+				if (m <= 0) cout << "Ошибка! Попробуйте снова!\n\n";
+			} while (m <= 0);
+			result = gravityForce(m);
+			cout << "Результат вычислений: " << result << "\n";
+			break;
+		case 2:
+			do
+				{
+                    			cout << "Введите массу (m): ";
+					cin >> m;
+                    			if (m <= 0) cout << "Ошибка! Попробуйте снова!\n\n";
+            			} while (m <= 0);
+				cout << "Введите ускорение (a): ";
+				cin >> a;
+				result = weightInLift(m, a);
+				cout << "Результаты вычислений: " << result << "\n";
+				break;
 		case 0:
 			cout << "Работа завершена!" << "\n";
 			break;
